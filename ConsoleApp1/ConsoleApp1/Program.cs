e15a615d236c291ef4364b9d9aea934e6f1af357
rozghon <37801476+Rozghon@users.noreply.github.com>
2020-09-16 20:28:35 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int S = 0; int a, b;
+
+            for (int i = 0; i < 3; i++)
+            {
+                b = i + 1;
+                Console.WriteLine("Введiть опiр " + b + "-го елемента:");
+                a = Convert.ToInt32(Console.ReadLine());
+                S += a;
+            }
+
+            Console.WriteLine("Опiр: " + S);
+        }
+    }
+}
