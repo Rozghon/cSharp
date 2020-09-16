53387cf94f0717c52ec0c3d1ef270500d298d512
rozghon <37801476+Rozghon@users.noreply.github.com>
2020-09-16 20:32:02 +0300

    Add files via upload

---
+++ Laba2/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double nn, nk, S = 1;
+
+            nn = Convert.ToDouble(Console.ReadLine());
+            nk = Convert.ToDouble(Console.ReadLine());
+
+            for (int i = Convert.ToInt32(nn); i <= Convert.ToInt32(nk); i++)
+            {
+                S *= (Math.Pow(i, 2) - (Math.Pow(-1, (Math.Pow(i, 2) + 1) * i))) / (Math.Pow(i, 2) + 2);
+            }
+
+            Console.WriteLine(S);
+        }
+}
