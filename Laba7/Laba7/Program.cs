08578a2778287237b5e6b705b25aaaf3e28e5596
rozghon <37801476+Rozghon@users.noreply.github.com>
2020-11-21 22:16:59 +0200

    Add files via upload

---
+++ Laba7/Laba7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Laba7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> collector = new List<string>();
+            bool isNewElement = true;
+
+            while (isNewElement)
+            {
+                Console.WriteLine("Do you want to enter a new item? Y N");
+                string inputString = Console.ReadLine();
+                if (inputString == "Y" || inputString == "y")
+                {
+                    Console.WriteLine("Enter a new item: ");
+                    collector.Add(Console.ReadLine());
+                }
+                else
+                {
+                    isNewElement = false;
+                }
+            }
+
+            int[] massive = new int[collector.Count];
+            for(int i = 0; i < collector.Count; i++)
+            {
+                massive[i] = collector[i].Length;
+            }
+            massiveElementCounter(massive);
+        }
+
+        static public void massiveElementCounter(int[] Array)
+        {
+            int Count = 0;
+            for (int i = 0; i < Array.Length; i++)
+            {
+                for (int j = 0; j < Array.Length; j++)
+                {
+                    if (Array[i] == Array[j])
+                        Count++;
+                }
+                Console.WriteLine("Element {0} occurs in the array {1} time", Array[i], Count);
+                Count = 0;
+            }
+        }
+    }
+}
